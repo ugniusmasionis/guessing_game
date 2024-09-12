ac5c09d357c4a1c580df7fca540e3e52e63ac48d
ugniusmasionis <admin@local.com>
2024-09-13 01:20:31 +0300

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,8 @@ fn main() {
 
         let mut guess = String::new();
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Не получилось прочитать строку");
+        let msg = "He получилось прочитать строку";
+        io::stdin().read_line(&mut guess).expect(msg);
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
